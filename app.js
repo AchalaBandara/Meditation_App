c2b6704e952f0b5c81cb5a4da6d7960d98003018
Achala Bandara <hmapb1994@gmail.com>
2020-05-23 16:05:01 +0530

    forth commit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = () => {
   const sounds = document.querySelectorAll(".sound-picker button");
   //time-display
   const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll(".time-select button");
   //get the length of the outline
   const outlineLength = outline.getTotalLength();
   //duration
@@ -16,9 +17,28 @@ const app = () => {
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
 
+  //pick different sounds
+  sounds.forEach(sound => {
+    sound.addEventListener("click", function() {
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      checkPlaying(song);
+    });
+  });
+
   //play sound
   play.addEventListener("click", () => {
     checkPlaying(song);
+  });
+
+  //select sound
+  timeSelect.forEach(option => {
+    option.addEventListener("click", function() {
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration % 60
+      )}`;
+    });
   });
 
   //create a function specific to stop and play the sounds
@@ -41,9 +61,19 @@ const app = () => {
     let seconds = Math.floor(elapsed % 60);
     let minutes = Math.floor(elapsed / 60);
 
+    //animate the text
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+
     //animate the circle
     let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
     outline.style.strokeDashoffset = progress;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = "./svg/play.svg";
+      video.pause();
+    }
   };
 };
 
